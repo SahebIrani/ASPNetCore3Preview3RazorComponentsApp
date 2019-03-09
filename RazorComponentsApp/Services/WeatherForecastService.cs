0f21484e41a1d15d692e7a2ce3cd86b5e4e6224a
Saheb Irani <sinjul.msbh@yahoo.com>
2019-03-09 14:25:36 +0330

    Add Paging Razor Grid

--- RazorComponentsApp/Services/WeatherForecastService.cs
+++ RazorComponentsApp/Services/WeatherForecastService.cs
@@ -4,22 +4,23 @@ using System.Threading.Tasks;
 
 namespace RazorComponentsApp.Services
 {
-    public class WeatherForecastService
-    {
-        private static string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+	public class WeatherForecastService
+	{
+		private static string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
+			"Sinjul","MSBH","Jack","Slater"
+		};
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
-        {
-            var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = startDate.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            }).ToArray());
-        }
-    }
+		public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+		{
+			var rng = new Random();
+			return Task.FromResult(Enumerable.Range(1, Summaries.Length).Select(index => new WeatherForecast
+			{
+				Date = startDate.AddDays(index),
+				TemperatureC = rng.Next(-20, 55),
+				Summary = Summaries[rng.Next(Summaries.Length)]
+			}).ToArray());
+		}
+	}
 }
